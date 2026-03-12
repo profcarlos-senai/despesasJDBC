4057635d1ea10b03a79f75756aa79b31e72ef201
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-03-12 20:18:02 -0300

    sepultado

--- src/main/java/util/ConexaoDB.java
+++ src/main/java/util/ConexaoDB.java
@@ -9,21 +9,30 @@ public class ConexaoDB {
 	private static final String URL = "jdbc:postgresql://ep-twilight-moon-acwrtu3o-pooler.sa-east-1.aws.neon.tech/aulas";
 	private static final String USUARIO = "senaipato";
 	private static final String SENHA = "SenaiPatoBranco";
-	private static Connection conexao = null;
+	private static int contador = 0;
 	
-	public static Connection getConexao(){
-		if (conexao == null) { // na primeira vez
-			try {
+	public static Connection getConexao() throws SQLException{
+		
+		if(contador++ == 0) {
+			try{
 				Class.forName("org.postgresql.Driver");
-				conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
-				System.out.println("Conexão deu boa");
 			}
-			catch(Exception ex) {
-				System.out.println("Deu ruim");
-				ex.printStackTrace();
+			catch(ClassNotFoundException ex) {
+				System.out.println("Não carregou o driver do PostgreSQL");
+				return null;
 			}
 		}
-		return conexao;
+		
+		try {
+			Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+			System.out.println("Conexão deu boa ("+contador+")");
+			return conexao;
+		}
+		catch(SQLException ex) {
+			System.out.println("Deu ruim ("+contador+")");
+			ex.printStackTrace();
+			throw ex; // toca a exceção pra cima
+		}
 	}
 	
 }
